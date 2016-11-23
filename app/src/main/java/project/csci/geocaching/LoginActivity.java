99f2c2885899c5ee72336a9282cf249f76527f46
Jeremy <jeremy.kwok@uoit.net>
2016-11-23 13:37:17 -0500

    Fixing Login flows. Changed main login page to have password hidden field

--- app/src/main/java/project/csci/geocaching/LoginActivity.java
+++ app/src/main/java/project/csci/geocaching/LoginActivity.java
@@ -3,22 +3,27 @@ package project.csci.geocaching;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.InputType;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
     private static final int REGISTER_CODE = 0;
     private UserDBHelper database;
-
+    EditText passwordText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         database = new UserDBHelper(this);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        passwordText = (EditText) findViewById(R.id.password_entry);
 
+        passwordText.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
     }
+
 
     public void sendLoginMessage(View view){
         EditText usernameEditText = (EditText) findViewById(R.id.username_entry);
@@ -28,6 +33,9 @@ public class LoginActivity extends AppCompatActivity {
             Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             intent.putExtra("username", usernameEditText.getText().toString());
             startActivity(intent);
+        }else{
+            //display toast
+            Toast.makeText(this, getString(R.string.password_dont_match), Toast.LENGTH_SHORT).show();
         }
     }
 
--- app/src/main/java/project/csci/geocaching/MainActivity.java
+++ app/src/main/java/project/csci/geocaching/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String username = getIntent().getStringExtra("username");
+        TextView usernameText = (TextView) findViewById(R.id.username_textview);
+        usernameText.setText(getString(R.string.welcome_message,username));
     }
 
     public void showCacheList(View view){
