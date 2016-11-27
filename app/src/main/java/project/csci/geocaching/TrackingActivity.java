738146c7f7e275a92f7d78f2492fc485019beb5a
Truyen Truong <truyen.truong@uoit.net>
2016-11-26 20:33:24 -0500

    Added successful claim message when claiming.

--- app/src/main/java/project/csci/geocaching/TrackingActivity.java
+++ app/src/main/java/project/csci/geocaching/TrackingActivity.java
@@ -248,7 +248,7 @@ public class TrackingActivity extends AppCompatActivity implements LocationListe
 
         Button claimButton = (Button)findViewById(R.id.claim_button);
 
-        if (distance <= 0.01) {
+        if (distance <= 0.5) {
             claimButton.setEnabled(true);
         } else {
             claimButton.setEnabled(false);
@@ -257,12 +257,14 @@ public class TrackingActivity extends AppCompatActivity implements LocationListe
 
     public void claimClicked(View view) {
         Intent output = new Intent();
+        output.putExtra("claimed", true);
         setResult(RESULT_OK,output);
         finish();
     }
 
     public void backClicked(View view) {
         Intent output = new Intent();
+        output.putExtra("claimed", false);
         setResult(RESULT_CANCELED,output);
         finish();
     }
